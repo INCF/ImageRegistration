37865006dcc93c57025823f56771ac8b2baf0f59
Luis Ibanez <luis.ibanez@kitware.com>
2011-04-29 19:13:39 -0400

    ENH: Placeholder for Gang scripts.
    Check wth Gang and UPenn...

---
+++ Scripts/FineVolumetricRegistration.sh
@@ -0,0 +1,11 @@
+#
+# Here we will use ANTS to perform the Deformable registration of slices versus
+# the 3D atlas.
+#
+#
+#   Put here the scripts that Gang developed at UPenn...
+#
+#
+# This will produce as output an set of CompositeTransforms (one per original slice)
+# each combining a Deformation field with several Affine Transforms.
+#
